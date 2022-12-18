80d8e4db5265bc1d28ee22292157c488c387a61f
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2022-12-18 13:00:29 +0900

    プロジェクトのCSSの設定をtailwindに変更

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,23 +4,23 @@ import Link from "next/link"
 import Image from "next/image"
 import { Category } from "../types/category"
 
-const Container = styled.div`
-  width: 100%;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 2rem;
-  padding-left: 2.5rem;
-  padding-right: 2.5rem;
-`
+// const Container = styled.div`
+//   width: 100%;
+//   margin-left: auto;
+//   margin-right: auto;
+//   margin-bottom: 2rem;
+//   padding-left: 2.5rem;
+//   padding-right: 2.5rem;
+// `
 
-const HeaderWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  padding-top: 2rem;
-  padding-bottom: 2rem;
-  border-bottom-width: 1px;
-  width: 100%;
-`
+// const HeaderWrapper = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   padding-top: 2rem;
+//   padding-bottom: 2rem;
+//   border-bottom-width: 1px;
+//   width: 100%;
+// `
 
 const Header: React.FC = () => {
   const matches = useMediaQuery("(min-width:768px)")
@@ -32,20 +32,15 @@ const Header: React.FC = () => {
     "culture"
   ]
   return (
-    <Container>
-      <HeaderWrapper>
+    <div className="container mx-auto px-10 mb-8">
+      <div className="flex align-middle justify-center md:justify-between border-b w-full border-blue-400 py-8">
         {matches ? (
           <Link href="/">
-            <Image
-              alt="logo"
-              height={30}
-              width={300}
-              src="/logo_lg.png"
-            ></Image>
+            <Image alt="logo" height={30} width={300} src="/logo.png"></Image>
           </Link>
         ) : (
           <Link href="/">
-            <Image alt="logo" height={100} width={400} src="/logo.png"></Image>
+            <Image alt="logo" height={30} width={300} src="/logo.png"></Image>
           </Link>
         )}
         {matches && (
@@ -57,8 +52,8 @@ const Header: React.FC = () => {
             ))}
           </div>
         )}
-      </HeaderWrapper>
-    </Container>
+      </div>
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {},
+  mode: "jit",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+}
