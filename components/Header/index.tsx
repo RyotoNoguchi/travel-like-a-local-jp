30fcd72f51c58122784c932faf08c93ecbed1cae
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2022-12-18 17:59:26 +0900

    HERO画画像変更

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,26 +1,7 @@
 import useMediaQuery from "@mui/material/useMediaQuery"
-import styled from "styled-components"
 import Link from "next/link"
 import Image from "next/image"
 import { Category } from "../types/category"
-
-// const Container = styled.div`
-//   width: 100%;
-//   margin-left: auto;
-//   margin-right: auto;
-//   margin-bottom: 2rem;
-//   padding-left: 2.5rem;
-//   padding-right: 2.5rem;
-// `
-
-// const HeaderWrapper = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   padding-top: 2rem;
-//   padding-bottom: 2rem;
-//   border-bottom-width: 1px;
-//   width: 100%;
-// `
 
 const Header: React.FC = () => {
   const matches = useMediaQuery("(min-width:768px)")
@@ -32,22 +13,24 @@ const Header: React.FC = () => {
     "culture"
   ]
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <div className="flex align-middle justify-center md:justify-between border-b w-full border-blue-400 py-8">
+    <div className="container mx-auto h-96 px-10 mb-8 bg-hero bg-no-repeat bg-cover bg-center bg-fixed">
+      <div className="flex justify-center md:justify-between w-full border-blue-400 py-8">
         {matches ? (
           <Link href="/">
-            <Image alt="logo" height={30} width={300} src="/logo.png"></Image>
+            <Image alt="logo" height={30} width={200} src="/logo.png"></Image>
           </Link>
         ) : (
           <Link href="/">
-            <Image alt="logo" height={30} width={300} src="/logo.png"></Image>
+            <Image alt="logo" height={30} width={200} src="/logo.png"></Image>
           </Link>
         )}
         {matches && (
-          <div>
+          <div className="flex items-center">
             {mockCategories.map((category) => (
               <Link key={category} href={`/category/${category}`}>
-                {category}
+                <span className="text-slate-500 ml-4 font-semibold cursor-pointer">
+                  {category}
+                </span>
               </Link>
             ))}
           </div>
