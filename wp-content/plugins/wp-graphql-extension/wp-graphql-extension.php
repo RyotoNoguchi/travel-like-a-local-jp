e135bca71529a2224d8b3b792a89f9a8359fba5e
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-02-09 23:21:52 +0900

    ポストのベージビューをqueryできることに成功

--- wp-content/plugins/wp-graphql-extension/wp-graphql-extension.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Plugin Name: WP GraphQL Extension
- * Plugin URI: https://www.wpgraphql.com
- * Version: 0.0.1-beta
- * Author: WPGraphQL
- * Author URI: https://www.wpgraphql.com
- * Description: This is an example plugin for extending WPGraphQL
- */
