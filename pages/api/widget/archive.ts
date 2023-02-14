5a04a54b68f7224438d294454eaf7b2b226ebcb2
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-02-14 09:08:21 +0900

    /api/widget/archive.ts作成

---
+++ pages/api/widget/archive.ts
@@ -0,0 +1,63 @@
+/* eslint-disable prettier/prettier */
+import axios, { AxiosResponse, AxiosError } from "axios"
+import type { NextApiRequest, NextApiResponse } from "next"
+import { GetPostsResponse } from "components/types/apiResponse"
+const API_URL = process.env.WORDPRESS_API_URL ?? ""
+
+const handler = async (
+  _: NextApiRequest,
+  res: NextApiResponse<{ month: string; count: number }[]>
+) => {
+  const options = {
+    method: "POST",
+    url: API_URL,
+    headers: { "Content-Type": "application/json" },
+    data: {
+      query: `#graphql 
+        query GetAllPosts {
+          posts {
+            edges {
+              node {
+                date
+              }
+            }
+          }
+        }
+      `
+    }
+  }
+
+  // axiosでGraphQLのAPIコールの仕方(https://rapidapi.com/guides/graphql-axios)
+  const data: GetPostsResponse = await axios
+    .request(options)
+    .then((res: AxiosResponse) => res.data)
+    .catch((err: AxiosError) => {
+      if (err.code === "ECONNABORTED") {
+        console.log("axios API call failed")
+      }
+    })
+  const dates = data?.data?.posts?.edges.map(({ node }) => node.date)
+
+  const datesReduceResult = dates.reduce<{ [key: string]: number }>(
+    (yearMonthCounts, gmt) => {
+      const yyyyMM = gmt.slice(0, 7)
+
+      if (!yearMonthCounts[yyyyMM]) {
+        yearMonthCounts[yyyyMM] = 0
+      }
+
+      yearMonthCounts[yyyyMM]++
+
+      return yearMonthCounts
+    },
+    {}
+  )
+
+  const postsPerMonth: { month: string; count: number }[] = Object.entries(
+    datesReduceResult
+  ).map(([month, count]) => ({ month, count }))
+
+  res.json(postsPerMonth)
+}
+
+export default handler
