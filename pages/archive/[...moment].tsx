8f67c792135d74dbdd1e9a03cd03a1fb5a273ab7
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-02-25 12:41:14 +0900

    /archive/[...moment].tsx追加

--- pages/archive/[...moment].tsx
+++ pages/archive/[...moment].tsx
@@ -1,2 +1,8 @@
 // TODO アーカイブ記事一覧画面用のパス ex.) '/archive/2023' or '/archive/2023/08'
 // TODO [...slug].tsxを参考にして作成する
+
+const ArchiveListPage: React.FC = () => {
+  return <></>
+}
+
+export default ArchiveListPage
