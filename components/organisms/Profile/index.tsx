dbe88dfd1e3f5a7f665a8339fd66cd3412a07acf
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-04-26 11:06:55 +0900

    ProfilePageの形実装完了

--- components/organisms/Profile/index.tsx
+++ components/organisms/Profile/index.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image"
+import { Key } from "swr"
+import { useSWRWithTimeout } from "components/hooks/swr"
+import { Author } from "components/types"
+
 const Profile: React.FC = () => {
+  const authorProfileKey: Key = "/api/author/profile"
+  const { data: profile, isValidating } =
+    useSWRWithTimeout<Author>(authorProfileKey)
+
+  if (!profile || isValidating) {
+    return null
+  }
   return (
     <>
       <h1>About Me</h1>
+      <Image
+        src={profile.avatar.url}
+        alt="author-portrait"
+        width={120}
+        height={120}
+        className="rounded-full"
+      />
     </>
   )
 }
