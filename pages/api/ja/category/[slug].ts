ab648903f3b58be1ed5533672b7cbab50c9550be
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-05-09 20:45:16 +0900

    'pages/api/ja/category/'追加

---
+++ pages/api/ja/category/[slug].ts
@@ -0,0 +1,58 @@
+/* eslint-disable prettier/prettier */
+import type { NextApiRequest, NextApiResponse } from "next"
+import { Post } from "components/types/post"
+import request, { gql } from "graphql-request"
+import { GraphQLError } from "graphql"
+const GRAPHQL_API_URL = process.env.WORDPRESS_API_URL ?? ""
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Post[]>) => {
+  try {
+    const { slug } = req.query
+
+    const queryGetPostsByCategory = gql`
+      query GetPostsByCategory($slug: String!) {
+        japanesePosts(where: { categoryName: $slug }) {
+          edges {
+            node {
+              slug
+              title
+              excerpt
+              date
+              categories {
+                edges {
+                  node {
+                    name
+                  }
+                }
+              }
+              featuredImage {
+                node {
+                  altText
+                  sourceUrl
+                }
+              }
+              author {
+                node {
+                  avatar {
+                    url
+                  }
+                  description
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+    const { japanesePosts } = await request<{
+      japanesePosts: { edges: { node: Post }[] }
+    }>(GRAPHQL_API_URL, queryGetPostsByCategory, { slug })
+    res.json(japanesePosts?.edges?.map(({ node }) => node))
+  } catch (error: unknown) {
+    const graphQLError = error as GraphQLError
+    console.log(graphQLError.message)
+  }
+}
+
+export default handler
