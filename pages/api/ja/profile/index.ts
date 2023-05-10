531df40cbb87a04a957209d928ee746cae9f1192
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-05-10 09:50:14 +0900

    日本語ページ用のAPI追加

---
+++ pages/api/ja/profile/index.ts
@@ -0,0 +1,47 @@
+/* eslint-disable prettier/prettier */
+import type { NextApiRequest, NextApiResponse } from "next"
+import request, { gql } from "graphql-request"
+import { GraphQLError } from "graphql"
+import { Author } from "components/types"
+const GRAPHQL_API_URL = process.env.WORDPRESS_API_URL ?? ""
+
+type GetProfileResponse = {
+  profile: Author & { content: string }
+}
+
+const handler = async (
+  _: NextApiRequest,
+  res: NextApiResponse<GetProfileResponse>
+) => {
+  try {
+    const queryGetProfile = gql`
+      query GetProfile {
+        page(id: "profile_jp", idType: URI) {
+          author {
+            node {
+              avatar {
+                url
+              }
+              description
+              firstName
+              lastName
+            }
+          }
+          title
+          content
+        }
+      }
+    `
+
+    const { page } = await request<{ page: GetProfileResponse }>(
+      GRAPHQL_API_URL,
+      queryGetProfile
+    )
+    res.json(page)
+  } catch (error: unknown) {
+    const graphQLError = error as GraphQLError
+    console.log(graphQLError.message)
+  }
+}
+
+export default handler
