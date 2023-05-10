7ed50c3302e3bdbd6a4ed7cc3e848e071752b5ff
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-05-10 11:50:00 +0900

    'pages/ja/category/'作成

--- components/organisms/CategoryPostCards/index.tsx
+++ components/organisms/CategoryPostCards/index.tsx
@@ -1,17 +1,18 @@
 import { useSWRDynamic } from "components/hooks/swr"
+import { useLanguage } from "components/hooks/useLanguage"
 import PostCard from "components/molecules/PostCard"
 import { Post } from "components/types"
 import { useRouter } from "next/router"
 
 const CategoryPostCards: React.FC = () => {
+  const { isEnglish } = useLanguage()
   const router = useRouter()
   const category = router.query.category
   const { data: postsData, isValidating } = useSWRDynamic<Post[]>(
-    "/api/en/category",
+    isEnglish ? "/api/en/category" : "/api/ja/category",
     category
   )
 
-  console.log("postsData:", postsData)
   if (isValidating) return null
   if (!postsData) return null
 
