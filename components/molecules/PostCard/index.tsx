9e459c8e03830b4100eeebef2a1fd7687e15714a
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-05-19 11:46:16 +0900

    stringのencodeをdecode

--- components/molecules/PostCard/index.tsx
+++ components/molecules/PostCard/index.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import { Post } from "components/types/post"
 import moment from "moment"
 import { useLanguage } from "components/hooks/useLanguage"
+import he from "he"
 
 const PostCard: React.FC<{ post: Post }> = ({ post }) => {
   const [featuredImage, setFeaturedImage] = useState(
@@ -29,55 +30,57 @@ const PostCard: React.FC<{ post: Post }> = ({ post }) => {
           className="object-center h-48 md:h-80 w-full object-cover shadow rounded-t-lg text-center"
         />
       </div>
-      <h1 className="transition duration-300 text-center cursor-pointer hover:text-pink-600 text-3xl font-semibold">
-        <Link
-          href={isEnglish ? `/en/post/${post.slug}` : `/ja/post/${post.slug}`}
-        >
-          {post.title}
-        </Link>
-      </h1>
-      <div className="block lg:flex text-center items-center justify-center mb-2 w-full">
-        <div className="flex items-center justify-center lg:mb-0 w-full lg:w-auto mr-8">
-          <Image
-            src={post.author?.node.avatar.url}
-            alt={post.author?.node.name}
-            width={30}
-            height={30}
-            className="align-middle rounded-full drop-shadow-sm shadow"
-          />
-          <p className="inline align-middle text-gray-700 ml-2 text-lg">
-            {post.author?.node.name}
-          </p>
-        </div>
-        <div className="font-medium text-gray-700 mt-1">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 inline mr-2 text-pink-500"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
+      <div className="p-2">
+        <h1 className="transition duration-300 text-center cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+          <Link
+            href={isEnglish ? `/en/post/${post.slug}` : `/ja/post/${post.slug}`}
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            {post.title}
+          </Link>
+        </h1>
+        <div className="block lg:flex text-center items-center justify-center mb-2 w-full">
+          <div className="flex items-center justify-center lg:mb-0 w-full lg:w-auto mr-8">
+            <Image
+              src={post.author?.node.avatar.url}
+              alt={post.author?.node.name}
+              width={30}
+              height={30}
+              className="align-middle rounded-full drop-shadow-sm shadow"
             />
-          </svg>
-          <span>{moment(post.date).format("MMM DD, YYYY")}</span>
+            <p className="inline align-middle text-gray-700 ml-2 text-lg">
+              {post.author?.node.name}
+            </p>
+          </div>
+          <div className="font-medium text-gray-700 mt-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 inline mr-2 text-pink-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+              />
+            </svg>
+            <span>{moment(post.date).format("MMM DD, YYYY")}</span>
+          </div>
         </div>
-      </div>
-      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
-        {post.excerpt.replace(/(<([^>]+)>)/gi, "")}
-      </p>
-      <div className="text-center">
-        <Link
-          href={isEnglish ? `/en/post/${post.slug}` : `/ja/post/${post.slug}`}
-        >
-          <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 mb-4 cursor-pointer">
-            Continue Reading
-          </span>
-        </Link>
+        <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">
+          {he.decode(post.excerpt.replace(/(<([^>]+)>)/gi, ""))}
+        </p>
+        <div className="text-center">
+          <Link
+            href={isEnglish ? `/en/post/${post.slug}` : `/ja/post/${post.slug}`}
+          >
+            <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 mb-4 cursor-pointer">
+              Continue Reading
+            </span>
+          </Link>
+        </div>
       </div>
     </div>
     // <div>
