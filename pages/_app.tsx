6bb4936b4192b0e4e6a966fa3f9691753feece07
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-05-27 20:00:06 +0900

    Vercelのアナリティクスコンポーネント追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.scss"
 import type { AppProps } from "next/app"
 import { Layout } from "components/index"
+import { Analytics } from "@vercel/analytics/react"
 import "@aamodtgroup/gutenberg-styles/style.css"
 import "@aamodtgroup/gutenberg-styles/theme.css"
 
 const App = ({ Component, pageProps }: AppProps) => (
   <Layout>
     <Component {...pageProps} />
+    <Analytics />
   </Layout>
 )
 
