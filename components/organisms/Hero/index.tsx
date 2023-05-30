be75c21021b4c809544d6d73ec86b3ee4e639c2b
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-05-30 10:12:50 +0900

    Heroの<Image/>のプロパティ変更

--- components/organisms/Hero/index.tsx
+++ components/organisms/Hero/index.tsx
@@ -2,13 +2,14 @@ import Image from "next/image"
 
 const Hero: React.FC = () => (
   <div className="sm:h-60 md:h-h_full w-screen md:mb-8 mb-6">
-    <Image
-      src="/img/pc-hero.jpg"
-      alt="hero-image"
-      width={1200}
-      height={500}
-      className="object-cover sm:h-60 md:h-h_full w-screen"
-    />
+    <div className="w-full h-full relative">
+      <Image
+        src="/img/pc-hero.jpg"
+        alt="hero-image"
+        fill
+        className="object-cover sm:h-60 md:h-h_full w-screen"
+      />
+    </div>
   </div>
 )
 
