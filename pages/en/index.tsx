8565f5e767bee76da7d68129fff3db89ad4afa9a
Ryoto Noguchi <ryoto.noguchi@asoview.co.jp>
2023-05-31 17:22:52 +0900

    buildエラー回避検証

--- pages/en/index.tsx
+++ pages/en/index.tsx
@@ -19,9 +19,9 @@ import { InstagramEmbed } from "react-social-media-embed"
 import { useEffect, useState } from "react"
 import useMediaQuery from "@mui/material/useMediaQuery"
 
-type Props = InferGetStaticPropsType<typeof getStaticProps>
+// type Props = InferGetStaticPropsType<typeof getStaticProps>
 
-const TopPage: NextPage<Props> = ({ fallback }) => {
+const TopPage = () => {
   const [isRendered, setIsRendered] = useState(false)
   const isMobile = useMediaQuery("(max-width:400px)")
 
@@ -42,26 +42,26 @@ const TopPage: NextPage<Props> = ({ fallback }) => {
       </Head>
       <main>
         <Hero />
-        <SWRConfig value={{ fallback }}>
-          <FeaturedPosts />
-          <div className="px-4">
-            <div className="grid grid-cols-1 md:grid-cols-12 md:gap-12 lg:m-8 md:m-6">
-              <div className="md:col-span-8 col-span-1">
-                {/* fallbackなしだと、レンダリング後にfetcherが叩かれるため、一瞬ブランクな状態が発生する。console.logしてリロードするとundefinedになることを確認できる */}
-                <PopularPostCards />
-              </div>
-              <div className="md:col-span-4 col-span-1 relative">
-                <div className="sticky top-8 mb-8">
-                  <AboutMe />
-                  <PostWidget />
-                  <ArchiveWidget />
-                  {isMobile && <CategoryWidget />}
-                  {isRendered && <InstagramEmbed url={INSTAGRAM_POST} />}
-                </div>
+        {/* <SWRConfig value={{ fallback }}> */}
+        <FeaturedPosts />
+        <div className="px-4">
+          <div className="grid grid-cols-1 md:grid-cols-12 md:gap-12 lg:m-8 md:m-6">
+            <div className="md:col-span-8 col-span-1">
+              {/* fallbackなしだと、レンダリング後にfetcherが叩かれるため、一瞬ブランクな状態が発生する。console.logしてリロードするとundefinedになることを確認できる */}
+              <PopularPostCards />
+            </div>
+            <div className="md:col-span-4 col-span-1 relative">
+              <div className="sticky top-8 mb-8">
+                <AboutMe />
+                <PostWidget />
+                <ArchiveWidget />
+                {isMobile && <CategoryWidget />}
+                {isRendered && <InstagramEmbed url={INSTAGRAM_POST} />}
               </div>
             </div>
           </div>
-        </SWRConfig>
+        </div>
+        {/* </SWRConfig> */}
       </main>
     </div>
   )
@@ -69,66 +69,66 @@ const TopPage: NextPage<Props> = ({ fallback }) => {
 
 export default TopPage
 
-type GetStaticPropsResponse = {
-  fallback: {
-    [key: string]: Post[] | string[] | Archive[] | string
-  }
-}
+// type GetStaticPropsResponse = {
+//   fallback: {
+//     [key: string]: Post[] | string[] | Archive[] | string
+//   }
+// }
 
-export const getStaticProps: GetStaticProps<
-  GetStaticPropsResponse
-> = async () => {
-  // featuredPosts取得
-  const getFeaturedPostsResponse = await axios.get<
-    Post[],
-    AxiosResponse<Post[]>
-  >("/posts/featured")
-  const featuredPosts = getFeaturedPostsResponse.data
+// export const getStaticProps: GetStaticProps<
+//   GetStaticPropsResponse
+// > = async () => {
+//   // featuredPosts取得
+//   const getFeaturedPostsResponse = await axios.get<
+//     Post[],
+//     AxiosResponse<Post[]>
+//   >("/posts/featured")
+//   const featuredPosts = getFeaturedPostsResponse.data
 
-  // recentPosts取得
-  const getRecentPostsResponse = await axios.get<Post[], AxiosResponse<Post[]>>(
-    "/posts/recent"
-  )
-  const recentPosts = getRecentPostsResponse.data
+//   // recentPosts取得
+//   const getRecentPostsResponse = await axios.get<Post[], AxiosResponse<Post[]>>(
+//     "/posts/recent"
+//   )
+//   const recentPosts = getRecentPostsResponse.data
 
-  // ウィジェット用のカテゴリー取得
-  const getCategoriesResponse = await axios.get<
-    string[],
-    AxiosResponse<string[]>
-  >("/category")
-  const categories = getCategoriesResponse.data
+//   // ウィジェット用のカテゴリー取得
+//   const getCategoriesResponse = await axios.get<
+//     string[],
+//     AxiosResponse<string[]>
+//   >("/category")
+//   const categories = getCategoriesResponse.data
 
-  // popularPosts取得
-  const getPopularPostsResponse = await axios.get<
-    Post[],
-    AxiosResponse<Post[]>
-  >("/posts/popular")
-  const popularPosts = getPopularPostsResponse.data
+//   // popularPosts取得
+//   const getPopularPostsResponse = await axios.get<
+//     Post[],
+//     AxiosResponse<Post[]>
+//   >("/posts/popular")
+//   const popularPosts = getPopularPostsResponse.data
 
-  // archivesを取得
-  const getArchiveWidgetResponse = await axios.get<
-    Archive[],
-    AxiosResponse<Archive[]>
-  >("/widget/archive")
-  const archives = getArchiveWidgetResponse?.data
+//   // archivesを取得
+//   const getArchiveWidgetResponse = await axios.get<
+//     Archive[],
+//     AxiosResponse<Archive[]>
+//   >("/widget/archive")
+//   const archives = getArchiveWidgetResponse?.data
 
-  // profile画像を取得
-  const getProfilePictureResponse = await axios.get<
-    string,
-    AxiosResponse<string>
-  >("/author/profile")
-  const profilePictureUrl = getProfilePictureResponse.data
+//   // profile画像を取得
+//   const getProfilePictureResponse = await axios.get<
+//     string,
+//     AxiosResponse<string>
+//   >("/author/profile")
+//   const profilePictureUrl = getProfilePictureResponse.data
 
-  return {
-    props: {
-      fallback: {
-        "/api/en/posts/featured": featuredPosts,
-        "/api/en/posts/recent": recentPosts,
-        "/api/en/posts/popular": popularPosts,
-        "/api/en/category": categories,
-        "/api/en/widget/archive": archives,
-        "/api/en/author/profile": profilePictureUrl
-      }
-    }
-  }
-}
+//   return {
+//     props: {
+//       fallback: {
+//         "/api/en/posts/featured": featuredPosts,
+//         "/api/en/posts/recent": recentPosts,
+//         "/api/en/posts/popular": popularPosts,
+//         "/api/en/category": categories,
+//         "/api/en/widget/archive": archives,
+//         "/api/en/author/profile": profilePictureUrl
+//       }
+//     }
+//   }
+// }
